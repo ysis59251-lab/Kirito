58d72ad079b4d0fba3e1aa86b72a83a66785c0a9
ysis59251-lab <ysis59251@gmail.com>
2026-01-17 09:32:27 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "ใส่ของคุณ",
+  authDomain: "ใส่ของคุณ",
+  databaseURL: "ใส่ของคุณ",
+  projectId: "ใส่ของคุณ",
+  storageBucket: "ใส่ของคุณ",
+  messagingSenderId: "ใส่ของคุณ",
+  appId: "ใส่ของคุณ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
